05069dfa581d0ab9962324bac939a4dd5dc6f180
Baraa Mohammad <baraa.mohd90@gmail.com>
2025-05-21 22:16:26 +0200

    "longest_common_prefix is added."

---
+++ longest_commom_prefix.py
@@ -0,0 +1,32 @@
+from bokeh.sampledata.iris import flowers
+
+
+class LongestCommonPrefix:
+    """
+    This class finds the longest common prefix
+    string amongst an array of strings.
+    """
+
+    def __init__(self, strs: list[str]):
+        self.strs = strs
+        self.longest_common_prefix = ""
+
+    def longestCommonPrefix(self) -> str:
+
+        if not self.strs:
+            return "The list is empty."
+        shortest_string = min(self.strs, key=len)
+        for i in range(len(shortest_string)):
+            prefix = shortest_string[:i+1]
+            if all(s.startswith(prefix) for s in self.strs):
+                self.longest_common_prefix = prefix
+            else:
+                break
+        if self.longest_common_prefix:
+            return f"The longest common prefix is '{self.longest_common_prefix}'."
+        else:
+            return "The words in the given list have no common list."
+
+obj = LongestCommonPrefix(["flower", "flight", "flow"])
+result = obj.longestCommonPrefix()
+print(result)
